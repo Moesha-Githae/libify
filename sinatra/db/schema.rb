81f926d450f134fe9ca562643c175aff13aa3af9
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-10 11:08:13 +0300

    update

--- sinatra/db/schema.rb
+++ sinatra/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_045352) do
+ActiveRecord::Schema.define(version: 2023_06_06_090831) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "description"
