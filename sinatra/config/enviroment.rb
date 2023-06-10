4a80e1e31d5e672dbe31160b8d26d6e90c3e920a
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-10 10:19:11 +0300

    update

--- sinatra/config/enviroment.rb
+++ sinatra/config/enviroment.rb
@@ -1,16 +1,24 @@
 
 
-ENV["RACK_ENV"] ||= "development"
+ENV['RACK_ENV'] ||= "production"
 
+# Require in Gems
 require 'bundler/setup'
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require(:default, ENV['RACK_ENV'])
 
-#database configuration
-configure do 
-    set :database,{
-        adapter: "sqlite3",
-        database: "db/development.sqlite3"
-    }
+# Database configurations
+configure do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://libify:GikvSmZBn3DeGAQGI8bhh1QJVLNzR2ev@dpg-ci220567avj2t326gufg-a.oregon-postgres.render.com/libify')
+  
+  set :database, {
+    adapter: 'postgresql',
+    host: db.host,
+    port: db.port,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1]
+  }
 end
 
-require_all "app"
+# Require all files in the 'app' directory
+require_all 'app'
